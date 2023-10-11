76045d52b51663470fefbde1a7ccea3c017f891d
Ravi <86828719+imraviprasad@users.noreply.github.com>
2023-10-11 18:08:29 +0530

    Add files via upload

---
+++ 08 -Adding-backend-code-and-api-routes/pages/feedback/index.js
@@ -0,0 +1,44 @@
+/** @format */
+import { Fragment, useState } from "react";
+import { buildFeedbackPath, extractFeedback } from "../api/feedback";
+
+function FeedbackPage(props) {
+  const [feedbackData, setFeedbackData] = useState();
+
+  function loadFeebackHandler(id) {
+    fetch(`/api/feedback/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackData(data.feedback);
+      });
+  }
+
+  return (
+    <Fragment>
+      {feedbackData && <p>{feedbackData.email}</p>}
+      <ul>
+        {props.feedbackItems.map((item) => (
+          <li key={item.id}>
+            {item.text}{" "}
+            <button onClick={loadFeebackHandler.bind(null, item.id)}>
+              Show Details
+            </button>
+          </li>
+        ))}
+      </ul>
+    </Fragment>
+  );
+}
+
+export async function getStaticProps() {
+  const filePath = buildFeedbackPath();
+  const data = extractFeedback(filePath);
+
+  return {
+    props: {
+      feedbackItems: data,
+    },
+  };
+}
+
+export default FeedbackPage;
